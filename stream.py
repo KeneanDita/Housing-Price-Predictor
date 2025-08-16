6f8442c139c47d7ad1de596d963b13e50656141b
Kenean Dita <KeneanSufa@gmail.com>
2025-08-16 18:17:15 +0300

    server ready for docker

--- stream.py
+++ stream.py
@@ -19,10 +19,20 @@ households = st.number_input("Households", value=400.0)
 median_income = st.number_input("Median Income (10k USD)", value=3.0, step=0.1)
 
 
-
 if st.button("Predict Price"):
-    input_data = np.array([[longitude, latitude, housing_median_age,
-                            total_rooms, total_bedrooms, population,
-                            households, median_income]])
+    input_data = np.array(
+        [
+            [
+                longitude,
+                latitude,
+                housing_median_age,
+                total_rooms,
+                total_bedrooms,
+                population,
+                households,
+                median_income,
+            ]
+        ]
+    )
     prediction = model.predict(input_data)[0]
     st.success(f"🏡 Estimated House Value: **${prediction:,.2f}**")
